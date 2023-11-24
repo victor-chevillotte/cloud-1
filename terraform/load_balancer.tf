17a4257cfdc41df8664f3e58111bb862f7e2c8ef
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-11-24 11:37:16 +0100

    feat/ added aws_lb_listener

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -68,3 +68,14 @@ resource "aws_lb_target_group_attachment" "wordpress_attachment" {
   target_id        = aws_instance.dev.id
   port             = 80
 }
+
+resource "aws_lb_listener" "wordpress_listener" {
+  load_balancer_arn = aws_lb.alb_wordpress.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_wordpress.arn
+  }
+}
