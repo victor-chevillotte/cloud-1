f686175007e723621bb9fee18adc28079606eb35
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-11-30 17:02:20 +0100

    Update origin protocol policy to "http-only" in cloud_front.tf

--- terraform/cloud_front.tf
+++ terraform/cloud_front.tf
@@ -5,7 +5,7 @@ resource "aws_cloudfront_distribution" "wordpress" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "https-only"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -83,30 +83,10 @@ resource "aws_lb_target_group_attachment" "wordpress_attachment" {
   port             = 80
 }
 
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.alb_wordpress.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-
-
-
-
 resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.alb_wordpress.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
