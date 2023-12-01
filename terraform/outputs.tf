ef52afe0fa500f13303754572bc334eb378c653c
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-01 17:48:53 +0100

    Remove unused output in terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,3 @@ output "instances_ip" {
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.wordpress.domain_name
 }
-
-output "cloudfront_id" {
-  value = aws_cloudfront_distribution.wordpress.viewer_certificate[0].acm_certificate_arn
-}
