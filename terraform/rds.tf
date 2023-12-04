d92c310a40fc78ef682569be1507f8a394de17e0
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-04 17:20:38 +0100

    fixed rds config

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,15 +1,15 @@
 resource "aws_db_instance" "wordpress" {
-  allocated_storage                   = 5
-  engine                              = "mysql"
-  engine_version                      = "5.7"
+  allocated_storage = 20
+  engine            = "mariadb"
+  storage_type      = "gp2"
+
+  engine_version                      = "10.6.14"
   instance_class                      = "db.t3.micro"
   db_name                             = var.db_name
   username                            = var.db_username
   password                            = var.db_password
-  parameter_group_name                = "default.mysql5.7"
   skip_final_snapshot                 = true
   identifier                          = "${var.prefix}-wordpress"
-  iam_database_authentication_enabled = true
   db_subnet_group_name                = aws_db_subnet_group.my_rds.name
   vpc_security_group_ids              = [aws_security_group.rds.id]
 
