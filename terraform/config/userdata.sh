205c270bf4eac2ee90da61c7cceec4bd82d8446d
mdesoeuvre <mehdi.desoeuvre@revolve.team>
2023-12-05 13:47:57 +0100

    feat: ip address in website name

--- terraform/config/userdata.sh
+++ terraform/config/userdata.sh
@@ -9,8 +9,9 @@ newgrp docker
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 mv /tmp/.env /home/ec2-user/.env
-mv /tmp/docker-compose.yaml /home/ec2-user/docker-compose.yaml
+echo "EC2_HOSTNAME=$HOSTNAME" >> /home/ec2-user/.env
 sudo chown ec2-user:ec2-user /home/ec2-user/.env
+mv /tmp/docker-compose.yaml /home/ec2-user/docker-compose.yaml
 sudo chown ec2-user:ec2-user /home/ec2-user/docker-compose.yaml
 sudo systemctl start wordpress.service
 sudo systemctl enable wordpress.service
