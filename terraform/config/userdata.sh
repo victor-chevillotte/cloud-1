e5c4ff02bf2bfe36c47dc48e75962154fb0dff42
mdesoeuvre <mehdi.desoeuvre@revolve.team>
2023-12-05 14:05:17 +0100

    fix: removed ip in website name

--- terraform/config/userdata.sh
+++ terraform/config/userdata.sh
@@ -9,7 +9,6 @@ newgrp docker
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 mv /tmp/.env /home/ec2-user/.env
-echo "EC2_HOSTNAME=$HOSTNAME" >> /home/ec2-user/.env
 sudo chown ec2-user:ec2-user /home/ec2-user/.env
 mv /tmp/docker-compose.yaml /home/ec2-user/docker-compose.yaml
 sudo chown ec2-user:ec2-user /home/ec2-user/docker-compose.yaml
