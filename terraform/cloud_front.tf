10e6ab049615a4816d91033bfb8e03c0363b9d46
mdesoeuvre <mehdi.desoeuvre@revolve.team>
2023-12-08 17:30:07 +0100

    feat: cache wip

--- terraform/cloud_front.tf
+++ terraform/cloud_front.tf
@@ -20,6 +20,7 @@ resource "aws_cloudfront_distribution" "cloud1" {
   default_cache_behavior {
     cache_policy_id  = aws_cloudfront_cache_policy.cloud1.id
     target_origin_id = aws_lb.alb_wordpress.id
+    
 
     allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -27,6 +28,20 @@ resource "aws_cloudfront_distribution" "cloud1" {
     viewer_protocol_policy   = "redirect-to-https"
     origin_request_policy_id = aws_cloudfront_origin_request_policy.cloud1.id
 
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/wp-content/uploads/*"
+    target_origin_id = aws_lb.alb_wordpress.id
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods  = ["GET", "HEAD", "OPTIONS"]
+    viewer_protocol_policy   = "redirect-to-https"
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.managed-allviewer.id
+    cache_policy_id = aws_cloudfront_cache_policy.cloud1.id
+    default_ttl = 30
+    max_ttl     = 60
+    min_ttl     = 1
   }
 
   restrictions {
@@ -72,27 +87,29 @@ resource "aws_cloudfront_origin_request_policy" "cloud1" {
   }
 }
 
+
+data "aws_cloudfront_origin_request_policy" "managed-allviewer" {
+  name = "Managed-AllViewer"
+}
+
+
 resource "aws_cloudfront_cache_policy" "cloud1" {
   name = "${var.prefix}-cache-policy"
 
-  default_ttl = 10
-  max_ttl     = 20
-  min_ttl     = 1
-
+  default_ttl = 30
+  max_ttl     = 60
+  min_ttl     = 10
   parameters_in_cache_key_and_forwarded_to_origin {
     cookies_config {
-      cookie_behavior = "all"
+      cookie_behavior = "none"
     }
 
     headers_config {
-      header_behavior = "whitelist"
-      headers {
-        items = ["Host"]
-      }
+      header_behavior = "none"
     }
 
     query_strings_config {
-      query_string_behavior = "none"
+      query_string_behavior = "all"
     }
   }
 }
